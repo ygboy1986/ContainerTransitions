78e3b3b0803ae75f7c6b0c8990d8012776479b49
OCBACK <zhangjiangbo2008@163.com>
2015-04-21 18:22:39 +0800

    添加视图标签
    1.添加视图标签
    2.添加通知

--- ContainerTransitions/YB_ChildViewController.m
+++ ContainerTransitions/YB_ChildViewController.m
@@ -30,13 +30,34 @@
     self.privateTitleLabel.backgroundColor = [UIColor clearColor];
     
     /**
-     * IOS 7 给出的6
+     * IOS 7 给出的6种字体样式
+     * UIFontTextStyleHeadline(其中一种)
      */
     self.privateTitleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline];
+    self.privateTitleLabel.textAlignment = NSTextAlignmentCenter;
+    self.privateTitleLabel.numberOfLines = 0;
+    [self.privateTitleLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    self.view = [[UIView alloc]init];
+    [self.view addSubview:self.privateTitleLabel];
+    
+    //添加视图标签
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.privateTitleLabel attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeWidth multiplier:0.6 constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.privateTitleLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
+
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.privateTitleLabel attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1 constant:0]];
+    
+
+    
+    
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.privateTitleLabel.text = self.title;
+    [self _updateAppearance];
+    //添加通知
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(_contentSizeCategoryDidChangeWithNotification) name:UIContentSizeCategoryDidChangeNotification object:nil];
 }
 
 #pragma mark - 私有方法
@@ -46,6 +67,11 @@
         self.view.backgroundColor = self.themeColor;
         self.view.tintColor = self.themeColor;
     }
+}
+-(void)_contentSizeCategoryDidChangeWithNotification:(NSNotification*)notification
+{
+    self.privateTitleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline];
+    [self.privateTitleLabel invalidateIntrinsicContentSize];
 }
 
 - (void)didReceiveMemoryWarning {
